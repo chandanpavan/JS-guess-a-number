e177dcffd741017d534148271b9b4f9e4528e78f
Chandan <chandu.h2024@gmail.com>
2026-03-09 03:03:29 +0530

    initial-final-commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
+
+const form = document.querySelector("form");
+const lastresult = document.querySelector("#lastresult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const previousguess = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const myArr = [];
+let cnt = 5;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const guessedNumber = parseInt(document.querySelector("#guessField").value);
+  if (cnt <= 0) {
+    lowOrHi.innerHTML = `Game Over`;
+    return;
+  }
+
+  cnt--;
+  lastResult.innerHTML = `${cnt}  `;
+
+  myArr.push(guessedNumber);
+  previousguess.innerHTML = `${myArr} `;
+
+  if (guessedNumber == randomNumber) {
+    lowOrHi.innerHTML = `<span> You have guessed the right number </span>`;
+    document.querySelector("#guessField").disabled = true;
+    document.querySelector("#subt").disabled = true;
+    return;
+  } else if (guessedNumber < randomNumber) {
+    lowOrHi.innerHTML = `<span>Your number is lower than Random Number </span>`;
+  } else {
+    lowOrHi.innerHTML = `<span>Your number is higher than Random Number</span>`;
+  }
+});
